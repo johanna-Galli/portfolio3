7d4eebbe30b9f0bf95e3ff3f0656993cf41145f5
Johanna Galli <galli.johanna.g2@gmail.com>
2020-03-02 09:23:44 +0100

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Johanna Galli</title>
+    <link href="asset/style/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <script src="asset/js/app.js"></script>
+</body>
+
+</html>
