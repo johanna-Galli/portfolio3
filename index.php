b4e971a62730ec16eb58ed1b162be0e201f59efd
Johanna Galli <galli.johanna.g2@gmail.com>
2020-04-15 14:57:33 +0200

    suppr overflow x

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Johanna Galli</title>
-    <link rel="icon" type="image/png" href="asset/img/favicon3.png" />
+    <link rel="icon" type="image/png" href="asset/img/favIcon3.png" />
     <link href="asset/style/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
