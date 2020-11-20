17f88078a96d81356fd5dd9624c42dcf1a177d56
unknown <galli.johanna.g2@gmail.com>
2020-11-20 17:49:52 +0100

    maj navbar scrollfunction

--- asset/js/navbar.js
+++ asset/js/navbar.js
@@ -3,119 +3,21 @@ let header = document.querySelector("header");
 let nav = document.querySelector("nav");
 let navLinks = document.querySelector(".navLinks");
 
+//tous les boutons
+let btns = document.getElementsByClassName("btn");
 
+//chaque bouton de la navbar
+let mebtn = document.getElementById("mebtn");
+let skillsbtn = document.getElementById("skillsbtn");
+let aboutbtn = document.getElementById("aboutbtn");
+let projectsbtn = document.getElementById("projectsbtn");
+let contactbtn = document.getElementById("contactbtn");
 
-//changement hauteur nav en fonction endroit sur le site
-window.onscroll = function () { scrollNavbar(); activeButtonScroll() };
-
-
-function scrollNavbar() {
-   //valeur du scroll top
-   scrollTop = window.scrollY;
-
-   //console.log(scrollTop);
-
-   //let padding = setInterval(frame, 10);
-
-   //let x = setTimeout(function() { alert("bip"); }, 2000);      
-   //console.log(x);
-
-
-   //taille navbar
-   if (scrollTop > header.scrollHeight) {
-      nav.style.padding = "5px 0";
-      nav.style.padding = "0";
-   }
-   if (scrollTop < header.scrollHeight) {
-      nav.style.padding = "10px 0";
-   }
-
-}
-
-function activeButtonScroll() {
-
-   let positionSectionSkills = header.scrollHeight;
-   let positionSectionAbout = sectionAbout.offsetTop;
-   let positionSectionProjects = sectionProjects.offsetTop;
-   let positionSectionContact = sectionContact.offsetTop;
-
-   //bouton actif au scroll
-   for (let i = 0; i < btns.length; i++) {
-
-      //bouton "me"
-      if (scrollTop == 0 || scrollTop < sectionSkills.scrollHeight) {
-         btns[i].classList.remove("active");
-         mebtn.className += " active";
-      }
-      //bouton "skills"
-      else if (scrollTop == positionSectionSkills || scrollTop > positionSectionSkills && scrollTop < (positionSectionAbout - 1)) {
-         btns[i].classList.remove("active");
-         skillsbtn.className += " active";
-      }
-      //bouton "about"
-      else if (scrollTop == (positionSectionAbout - 1) || scrollTop > positionSectionAbout && scrollTop < (positionSectionProjects - 1)) {
-         btns[i].classList.remove("active");
-         aboutbtn.className += " active";
-      }
-      //bouton "Projects"
-      else if (scrollTop == (positionSectionProjects - 1) || scrollTop > positionSectionProjects && scrollTop < (positionSectionContact - 1)) {
-         btns[i].classList.remove("active");
-         projectsbtn.className += " active";
-      }
-      //bouton "Contact"
-      else if (scrollTop == (positionSectionContact - 1) || scrollTop > (positionSectionContact - 1)) {
-         btns[i].classList.remove("active");
-         contactbtn.className += " active";
-      }
-   }
-}
-
-
-
-
-
-/*
-// fonction au scroll
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    //valeur du scroll top
-    let scrollTop = document.documentElement.scrollTop;
-
-    if (scrollTop > 10) {
-        console.log(scrollTop);
-        console.log("supp a 10");
-        //nav.style.backgroundColor = "black";
-    } else if (scrollTop >= 0 || scrollTop <= 10) {
-        //nav.style.backgroundColor = "none";
-        console.log("inf a 10");
-    }
-}
-
-// action fonction
-navBackground();
-*/
-
-
-/**********NAVBAR**********/
-
-/*
-window.onresize = function () {
-   if (window.innerWidth >= 992) {
-      toggleNav.style.display = 'block';
-   } else {
-      toggleNav.style.display = 'none';
-   }
-}
-
-function togglebutton() {
-   let toggleNav = document.getElementById('toggleNav');
-   if (toggleNav.style.display === 'none') {
-      toggleNav.style.display = 'block';
-   } else {
-      toggleNav.style.display = 'none';
-   }
-}
+//chaque section :
+const sectionSkills = document.getElementById("skills");
+const sectionAbout = document.getElementById("about");
+const sectionProjects = document.getElementById("projects");
+const sectionContact = document.getElementById("contact");
 
 //changement hauteur nav en fonction endroit sur le site
 window.onscroll = function () { scrollFunction() };
@@ -140,10 +42,6 @@ function scrollFunction() {
    let positionSectionProjects = sectionProjects.offsetTop;
    let positionSectionContact = sectionContact.offsetTop;
 
-   //console.log(positionSectionAbout);
-   //console.log(positionSectionAbout + 10);
-   //console.log(scrollTop);
-
    //bouton actif au scroll
    for (let i = 0; i < btns.length; i++) {
 
@@ -174,4 +72,3 @@ function scrollFunction() {
       }
    }
 }
-*/
