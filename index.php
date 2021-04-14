dbe95f8b60036bcb678e202416b18bb13b70e2fd
unknown <galli.johanna.g2@gmail.com>
2021-04-14 15:56:40 +0200

    ajout carousel projets

--- index.php
+++ index.php
@@ -323,164 +323,25 @@
     </section>
 
     <!--------------------------------------------------Projects-------------------------------------------------->
+
     <section id="projects" class="pt-4" data-scroll>
+        <h2 class="text-center">Réalisations web</h2>
+        <a href="realisations.php">
+            <div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true, "autoPlay": true }'>
+                <div class="carousel-cell">
 
-        <!--projets web-->
-        <div class="container100">
-            <h2 class="text-center">Projets web</h2>
+                    <img src="asset/img/projets/sites/convertisseur-d-age.jpg" alt="site web convertisseur d'age">
 
-            <!--projets web part 1-->
-            <div class="containerProjects">
-                <!--ATM-->
-                <div class="col-sm linkProject p-0 m-4">
-
-                    <img src="asset/img/projets/atm.JPG" alt="ATM" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">ATM</h4>
-                        <a href="https://github.com/johanna-Galli/atm" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet solo</h6>
-                            <h7>PHP - CSS - JS</h7>
-                        </div>
-                    </div>
                 </div>
-
-                <!--JENT-->
-                <div class="col-sm linkProject p-0 m-4">
-
-                    <img src="asset/img/projets/jentstrasbourg.jpg" alt="JENT Strasbourg" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Utilisation d'une API</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet de groupe (4)</h6>
-                            <h7>Symfony<br>PHP - SCSS - TWIG</h7>
-                        </div>
-                    </div>
+                <div class="carousel-cell">
+                    <img src="asset/img/projets/sites/convertisseur-d-age.jpg" alt="site web convertisseur d'age">
                 </div>
-
-                <!--ALLO JATI-->
-                <div class="col-sm linkProject p-0 m-4">
-                    <img src="asset/img/projets/allojati.JPG" alt="allo jati" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Création d'un site type : "Allo Ciné"</h4>
-                        <a href="https://github.com/johanna-Galli/allo_jati" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet de groupe (4)</h6>
-                            <h7>MVC - POO<br>PHP - CSS - TWIG - MYSQL</h7>
-                        </div>
-                    </div>
+                <div class="carousel-cell">
+                    <img src="asset/img/projets/sites/convertisseur-d-age.jpg" alt="site web convertisseur d'age">
                 </div>
-
-                <!--YES TRANSFERT-->
-                <div class="col-sm linkProject p-0 m-4">
-                    <img src="asset/img/projets/yestransfert.JPG" alt="yes transfert" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Création d'un site type : "WeTransfer"</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet de groupe (4)</h6>
-                            <h7>MVC <br> PHP - CSS - MYSQL</h7>
-                        </div>
-                    </div>
-                </div>
-
-                <!--BLUEASY-->
-                <div class="col-sm linkProject p-0 m-4">
-                    <img src="asset/img/projets/blueasy.JPG" alt="blueasy" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Intégration maquette</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet de groupe (2)</h6>
-                            <h7>HTML - CSS - Bootstrap</h7>
-                        </div>
-                    </div>
-                </div>
-
-
-
-                <!--projets web part 2-->
-
-
-
-                <!--MOCKUP-->
-                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
-
-                    <img src="asset/img/projets/mockup.JPG" alt="mockup" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Intégration maquette</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet de groupe (2)</h6>
-                            <h7>HTML - CSS - Bootstrap</h7>
-                        </div>
-                    </div>
-                </div>
-
-                <!--Convertisseur d'âge-->
-                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
-                    <img src="asset/img/projets/age.JPG" alt="age" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Convertir son âge</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet solo</h6>
-                            <h7>HTML - CSS - JS</h7>
-                        </div>
-                    </div>
-                </div>
-
-                <!--DEVINER NOMBRE-->
-                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
-                    <img src="asset/img/projets/guessNumber.JPG" alt="Guess number" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Jeu : deviner un nombre</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet solo</h6>
-                            <h7>HTML - CSS - JS</h7>
-                        </div>
-                    </div>
-                </div>
-
-                <!--MULTIPLICATION-->
-                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
-                    <img src="asset/img/projets/multiplication.JPG" alt="multiplication" class="imageWeb">
-                    <div class="overlayWeb">
-                        <h4 class="titleProject">Apprendre les tables de multiplication</h4>
-                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
-                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
-                        </a>
-                        <div class="infoProject">
-                            <h6>Projet solo</h6>
-                            <h7>HTML - CSS - JS</h7>
-                        </div>
-                    </div>
-                </div>
-
 
             </div>
-        </div>
-        <!--BOUTONS VOIR PLUS/MOINS -->
-        <button class="buttonProject viewMoreWeb">Voir plus</button>
-        <button class="buttonProject viewLessWeb">Voir moins</button>
+        </a>
 
         <!--projets graphisme-->
         <div class="container100">
---
+++ realisations.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Johanna Galli</title>
+
+    <!-- icon -->
+    <link rel="icon" type="image/png" href="asset/img/favIcon3.png" />
+
+    <!-- style -->
+    <link href="asset/style/style.css" rel="stylesheet">
+    <!-- style navbar -->
+    <link rel="stylesheet" href="asset/style/navbar.css">
+
+    <!-- style projects -->
+    <link rel="stylesheet" href="asset/style/projects.css">
+
+    <!-- style footer -->
+    <link rel="stylesheet" href="asset/style/footer.css
+    ">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="asset/style/font-awesome/font-awesome.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="asset/style/bootstrap/bootstrap.min.css">
+    <!-- Carousel Flickity-->
+    <link rel="stylesheet" href="asset/style/flickity/flickity.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+    <!--Navbar-->
+    <nav class="topnav topnavWhite">
+        <!--Liens-->
+        <ul class="navLinks">
+            <li>
+                <a href="#skills" id="skillsbtn" class="btn">Compétences</a>
+            </li>
+            <li>
+                <a href="#about" id="aboutbtn" class="btn">À propos</a>
+            </li>
+            <li>
+                <a href="#projects" id="projectsbtn" class="btn">Réalisations</a>
+            </li>
+            <li>
+                <a href="#contact" id="contactbtn" class="btn">Me contacter</a>
+            </li>
+        </ul>
+    </nav>
+
+
+
+    <!--------------------------------------------------Projects-------------------------------------------------->
+
+
+    <div class="main-carousel">
+        <div class="carousel-cell">...</div>
+        <div class="carousel-cell">...</div>
+        <div class="carousel-cell">...</div>
+        ...
+    </div>
+
+
+    <section id="projects" class="pt-4" data-scroll>
+
+        <!--projets web-->
+        <div class="container100">
+            <h2 class="text-center">Projets web</h2>
+
+            <!--projets web part 1-->
+            <div class="containerProjects">
+                <!--ATM-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/atm.JPG" alt="ATM" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">ATM</h4>
+                        <a href="https://github.com/johanna-Galli/atm" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet solo</h6>
+                            <h7>PHP - CSS - JS</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--JENT-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/jentstrasbourg.jpg" alt="JENT Strasbourg" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Utilisation d'une API</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet de groupe (4)</h6>
+                            <h7>Symfony<br>PHP - SCSS - TWIG</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--ALLO JATI-->
+                <div class="col-sm linkProject p-0 m-4">
+                    <img src="asset/img/projets/allojati.JPG" alt="allo jati" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Création d'un site type : "Allo Ciné"</h4>
+                        <a href="https://github.com/johanna-Galli/allo_jati" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet de groupe (4)</h6>
+                            <h7>MVC - POO<br>PHP - CSS - TWIG - MYSQL</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--YES TRANSFERT-->
+                <div class="col-sm linkProject p-0 m-4">
+                    <img src="asset/img/projets/yestransfert.JPG" alt="yes transfert" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Création d'un site type : "WeTransfer"</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet de groupe (4)</h6>
+                            <h7>MVC <br> PHP - CSS - MYSQL</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--BLUEASY-->
+                <div class="col-sm linkProject p-0 m-4">
+                    <img src="asset/img/projets/blueasy.JPG" alt="blueasy" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Intégration maquette</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet de groupe (2)</h6>
+                            <h7>HTML - CSS - Bootstrap</h7>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <!--projets web part 2-->
+
+
+
+                <!--MOCKUP-->
+                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
+
+                    <img src="asset/img/projets/mockup.JPG" alt="mockup" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Intégration maquette</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet de groupe (2)</h6>
+                            <h7>HTML - CSS - Bootstrap</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Convertisseur d'âge-->
+                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
+                    <img src="asset/img/projets/age.JPG" alt="age" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Convertir son âge</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet solo</h6>
+                            <h7>HTML - CSS - JS</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--DEVINER NOMBRE-->
+                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
+                    <img src="asset/img/projets/guessNumber.JPG" alt="Guess number" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Jeu : deviner un nombre</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet solo</h6>
+                            <h7>HTML - CSS - JS</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--MULTIPLICATION-->
+                <div class="col-sm linkProject p-0 m-4 suiteProjectsWeb">
+                    <img src="asset/img/projets/multiplication.JPG" alt="multiplication" class="imageWeb">
+                    <div class="overlayWeb">
+                        <h4 class="titleProject">Apprendre les tables de multiplication</h4>
+                        <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                            <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                        </a>
+                        <div class="infoProject">
+                            <h6>Projet solo</h6>
+                            <h7>HTML - CSS - JS</h7>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <!--BOUTONS VOIR PLUS/MOINS -->
+        <button class="buttonProject viewMoreWeb">Voir plus</button>
+        <button class="buttonProject viewLessWeb">Voir moins</button>
+
+        <!--projets graphisme-->
+        <div class="container100">
+            <h2 class="text-center">Visuels et graphismes</h2>
+
+            <!--projets graphisme part 1-->
+            <div class="containerProjects">
+                <!--ATM-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/logoatm.png" alt="ATM logo" class="imageGraphisme">
+                    <div class="overlayGraphisme">
+                        <h4 class="titleProject">Logo ATM</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Illustrator <br> Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Aabil-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/aabiltaxi.jpg" alt="Carte visite Aabil Taxi Mulhouse" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Carte de visite Aabil Taxi</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--cse-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/CSE.png" alt="Logo CSE Soléa" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Logo CSE Soléa</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--joal-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/joal.jpg" alt="Logo Joal & Co" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Logo Joal & Co</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--FUT-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/carte-fut.jpg" alt="Carte FUT LKR" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Carte FUT Lustige Klique</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--FUT-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/misslkr.png" alt="Logo miss LKR" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Logo miss Lustige Klique</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Ringispil 1-->
+                <div class="col-sm linkProject p-0 m-4">
+
+                    <img src="asset/img/projets/ringispil1.jpg" alt="DVD Ringispil" class="imageGraphisme">
+                    <div class="overlayGraphisme my-auto">
+                        <h4 class="titleProject">Pochette CD</h4>
+                        <div class="text-center text-white mt-3">
+                            <h7>Adobe Photoshop</h7>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--BOUTONS VOIR PLUS/MOINS 
+    <button class="buttonProject viewMoreWeb">Voir plus</button>
+    <button class="buttonProject viewLessWeb">Voir moins</button>-->
+
+            <!--projets web part 2
+    <div class="containerProjects suiteProjectsWeb">-->
+            <!--BLUEASY
+        <div class="col-sm linkProject p-0 m-4">
+            <img src="asset/img/projets/blueasy.JPG" alt="blueasy" class="imageWeb">
+            <div class="overlayWeb">
+                <h4 class="titleProject">Intégration maquette</h4>
+                <a href="https://github.com/johanna-Galli/JENT-Strasbourg" target="_blank">
+                    <img src="asset/img/loupe-white.png" alt="loupe" class="loupe">
+                </a>
+                <div class="infoProject">
+                    <h6>Projet de groupe (2)</h6>
+                    <h7>HTML - CSS - Bootstrap</h7>
+                </div>
+            </div>
+        </div>
+    </div>-->
+
+        </div>
+
+    </section>
+
+
+
+
+
+    <!--------------------------------------------------Footer-------------------------------------------------->
+    <p class="footer">Design et intégration : Johanna Galli - <a href="mentionsLegales.html">Mentions légales</a></p>
+</body>
+
+
+<!-- script scroll -->
+<script src="asset/js/scrollout/scroll-out.min.js"></script>
+<!-- script Flickity -->
+<script src="asset/js/flickity/flickity.pkgd.min.js"></script>
+<!-- script -->
+<script src="asset/js/app.js"></script>
+
+</html>
